cbe39ecfc754cf83f5be8919165a129cec5fc410
Tigerryttaren <civilaranton@gmail.com>
2013-09-30 16:02:07 +0200

    Dummynode support fixed

--- FlaskManagementConsole/blueprints/add_new_node.py
+++ FlaskManagementConsole/blueprints/add_new_node.py
@@ -3,15 +3,23 @@ from os.path import isfile, isdir, join, abspath, getmtime, getsize
 from datetime import datetime
 from flask import Flask, render_template, Blueprint, request, redirect, make_response
 from werkzeug import secure_filename
+import uuid
 
 management_console = Blueprint('add_new_node', __name__, template_folder='../templates')
 node_list = []
 
+
 class Node(object):
+	#TODO: Which fields are a node going to have? 
 	id = ""
+	port = 0
 	synced = False
-	#TODO: Fill with serious fields
-	cats = 0
+	
+	def __init__(self, id, port, synced):
+		self.id = id
+		self.port = port
+		self.synced = synced
+	
 # ===
 # Redirects
 
@@ -20,7 +28,7 @@ def index():
 	return redirect('/nodes')
 
 # ===
-# === Real Paths
+# === Actual Paths
 
 @management_console.route('/nodes')
 def list_nodes():
@@ -30,6 +38,12 @@ def list_nodes():
 @management_console.route('/nodes/add', methods=['GET', 'POST'])
 def add_node():
 	#TODO: Implement
+	if request.method == 'POST':
+		port = request.form.get('port_number')
+		id = uuid.uuid4()
+		status = False
+		node_list.append(Node(id, port, status))
+		return redirect('/nodes')
 	return render_template('addNode.html')
 
 @management_console.route('/nodes/<int:node_id>', methods=['GET'])
@@ -39,9 +53,6 @@ def remove_node():
 
 # ===
 
+# ===
+# Support Functions
 
-
-
-
-
- 
