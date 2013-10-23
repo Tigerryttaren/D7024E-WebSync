159fe531616c1e355fdfcaf74c6cd952ad2bc38c
Tigerryttaren <antwen-9@student.ltu.se>
2013-10-23 13:28:02 +0000

    Started with OpenStack Instances

--- FlaskManagementConsole/blueprints/add_new_node.py
+++ FlaskManagementConsole/blueprints/add_new_node.py
@@ -14,6 +14,7 @@ from os import system
 
 management_console = Blueprint('add_new_node', __name__, template_folder='../templates')
 node_list = []
+instance_list = []
 docker_image_name=""
 #current_ip = ""
 
@@ -28,7 +29,33 @@ class Node(object):
 		self.id = id
 		self.port = port
 		self.synced = synced
+
+class Instance(object):
+	#TODO Which field should an instance have?
+
+	id = ""
+	ip = ""
+	number = 0
+
+	def __init__(self, id, ip, number):
+		self.id = id
+		self.ip = ip
+		self.number = number
 	
+
+
+
+# BOTO stuff and things related to those stuffs
+image_name = "ubuntu-12.04-lts-x86_64" # add name of standard everytime run image from OpenStack
+boto_region = boto.ec2.regioninfo.RegionInfo(name="nova", endpoint="130.240.233.106")
+boto_conn = boto.connect_ec2(
+aws_access_key_id = "7a2da111b5934c14be7fa1f2b45ea45d",
+aws_secret_access_key = "507d4bb12b614b2f8c36648564ef8fbe",
+is_secure = False, 
+region = boto_region,
+port = 8773,
+path = "/services/Cloud")
+
 # ===
 # Redirects
 
@@ -39,8 +66,50 @@ def index():
 # ===
 # === Actual Paths
 
+
+
+# === EXPERIMENT WITH OPENSTACK
+@management_console.route('/instances')
+def list_instances():
+	# get instance list from OpenStack somehow?
+	return render_template('instanceList.html')
+	
+@management_console.route('/instances/add')
+def add_instance():
+	# create vm instance
+	# create floating ip
+	# associate floating ip
+	# ...
+	# profit?
+
+	response = boto_conn.run_instances(
+	image_name,
+	key_name = "websync",
+	instance_type = "m1.tiny",
+	security_groups = ["websync"],
+	)
+
+	address = boto_conn.allocate_address()
+	boto_conn.associate_address(instance_id=response, allocation_id=address)
+	
+	#instance_id = 
+	#instance_ip = 
+	#instace_number = 3
+	
+	#instance_list.append(Instance(instance_id, instace_ip, instace_number))
+
+	# Make instance object and add to list
+	# get floaitng ip and id
+	# 
+	
+	return redirect('/instances')
+	
+
 @management_console.route('/nodes')
 def list_nodes():
+	# change path to /instances/<instande_id>/nodes
+	# add ssh in to instance from URL and then run all this shit?
+
 	output = subprocess.check_output(["sudo", "docker", "ps"])
 	output = output.split('\n')
 	output.pop(0)				# removes first item due to was only headers
