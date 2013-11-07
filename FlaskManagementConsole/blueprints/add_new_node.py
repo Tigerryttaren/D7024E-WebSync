c00fe00e6ac61260ebd03fb8e09e12d2eb7d6e3c
Tigerryttaren <antwen-9@student.ltu.se>
2013-11-07 13:20:20 +0000

    Can list and remove instances now

--- FlaskManagementConsole/blueprints/add_new_node.py
+++ FlaskManagementConsole/blueprints/add_new_node.py
@@ -10,16 +10,13 @@ import boto
 import boto.ec2
 
 from os import system
-#import uuid
 
 management_console = Blueprint('add_new_node', __name__, template_folder='../templates')
 node_list = []
 instance_list = []
 docker_image_name=""
-#current_ip = ""
 
 class Node(object):
-	#TODO: Which fields are a node going to have? 
 	id = ""
 	port = 0
 	ip = ""
@@ -31,23 +28,15 @@ class Node(object):
 		self.synced = synced
 
 class Instance(object):
-	#TODO Which field should an instance have?
-
 	id = ""
 	ip = ""
-	number = 0
 
-	def __init__(self, id, ip, number):
+	def __init__(self, id, ip):
 		self.id = id
 		self.ip = ip
-		self.number = number
-	
-
-
 
 # BOTO stuff and things related to those stuffs
-image_name = "ubuntu-12.04-lts-x86_64" # add name of standard everytime run image from OpenStack
-
+image_name = "websync_snap"
 boto_region = boto.ec2.regioninfo.RegionInfo(name="nova", endpoint="130.240.233.106")
 boto_conn = boto.connect_ec2(
 aws_access_key_id = "7a2da111b5934c14be7fa1f2b45ea45d",
@@ -58,19 +47,22 @@ port = 8773,
 path = "/services/Cloud")
 
 # ===
-# Redirects
+# Root Redirect
 
 @management_console.route('/')
 def index():
-	return redirect('/nodes')
+	return redirect('/instances')
 
 # ===
 # === Actual Paths
 
 @management_console.route('/instances')
 def list_instances():
-	#TODO: Fix functionality for displaying the instances	
-	return render_template('instanceList.html')
+	#TODO: Fix functionality for displaying the instances
+		
+	#print "LIST SIZE: ", len(instance_list)
+		
+	return render_template('instanceList.html', instance_list=instance_list)
 	
 @management_console.route('/instances/add')
 def add_instance():
@@ -78,8 +70,8 @@ def add_instance():
 	# Starts the instance on OpenStack
 	response = boto_conn.run_instances(
 	image_name,
-	key_name = "websync",
-	instance_type = "m1.tiny",
+	key_name = "websync_base",
+	instance_type = "m1.small",
 	security_groups = ["websync"],
 	)
 	
@@ -88,20 +80,69 @@ def add_instance():
 			instance.update()
 	inst = response.instances[0]
 	
-	# Adding it to the display list
-	instance_list.append(inst) 
-
+	# Allocate a floating IP to instance
 	address_alloc = boto_conn.allocate_address()
 	address = (str(address_alloc).split(":"))[1]	
-
 	boto_conn.associate_address(inst, address)
 	
-	#TODO: ADD FUCTIONALITY FOR DISPLAYING THE INSTANCES
+	id = inst.id 
+	ip = address
+
+	instance_list.append(Instance(id, ip))
+
+	#print "LIST SIZE: ", len(instance_list)
 	
 	return redirect('/instances')
 
-@management_console.route('/in')
+@management_console.route('/instances/<string:instance_id>', methods=['GET'])
+def instance_info(instance_id):
+	for instance in instance_list:
+		if instance.id == instance_id:
+			return render_template('instanceInfo.html', instance=instance)
+		else:
+			pass
+	return render_template('instanceInfo.html')	
 	
+@management_console.route('/instances/<string:instance_id>/<string:instance_ip>/delete', methods=['GET', 'DELETE'])
+def delete_instance(instance_id, instance_ip):
+	
+	instances = [instance_id]
+	
+	if(boto_conn.disassociate_address(instance_ip)):
+		boto_conn.terminate_instances(instances)
+		boto_conn.release_address(instance_ip)
+
+        for instance in instance_list:
+                if instance.id == instance_id:
+                        instance_list.remove(instance)
+                else:
+                        pass
+
+        return redirect('/instances')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 @management_console.route('/nodes')
 def list_nodes():
