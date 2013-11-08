8f3bfe7707f294fd796b379c3d53a4820440d6e5
Tigerryttaren <antwen-9@student.ltu.se>
2013-11-08 16:11:51 +0000

    changed snapshot to be run

--- FlaskManagementConsole/blueprints/add_new_node.py
+++ FlaskManagementConsole/blueprints/add_new_node.py
@@ -36,7 +36,7 @@ class Instance(object):
 		self.ip = ip
 
 # BOTO stuff and things related to those stuffs
-image_name = "websync_snap"
+image_name = "websync_base"
 boto_region = boto.ec2.regioninfo.RegionInfo(name="nova", endpoint="130.240.233.106")
 boto_conn = boto.connect_ec2(
 aws_access_key_id = "7a2da111b5934c14be7fa1f2b45ea45d",
